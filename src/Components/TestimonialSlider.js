08ab8bab313474b8bfa76b796d2c157b97bf7551
1516Sumit <sumitsharma781425@gmail.com>
2023-10-23 20:45:11 +0530

    add testimonial and footer

--- src/Components/TestimonialSlider.js
+++ src/Components/TestimonialSlider.js
@@ -1,251 +1,180 @@
-import React, { Component } from "react";
-import Slider from "react-slick";
+
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import "swiper/swiper.min.css";
+import "swiper/modules/effect-coverflow/effect-coverflow.min.css";
+import "swiper/modules/pagination/pagination.min.css";
+import "swiper/modules/navigation/navigation.min.css";
 
 import "./testimonial.css";
 
-import "./slick.css";
-import "./slick-theme.css";
 
+import { Autoplay, EffectCoverflow, Pagination } from "swiper";
 
-export default class TestimonialSlider extends Component {
-  render() {
-    // const settings = {
-    //   dots: true,
-    //   infinite: true,
-    //   speed: 500,
-    //   slidesToShow: 3,
-    //   slidesToScroll: 1,
-    //   autoplay: true,
-    //   autoplaySpeed: 5000
-   
-    // };
+export default function App() {
+  return (
+    <>
+    <div className='testimonial'>
+      <Swiper
+        effect={'coverflow'}
+        grabCursor={true}
+        autoplay={{
+                        delay: 2500,
+                        disableOnInteraction: false,
+                    }}
+        centeredSlides={true}
+        slidesPerView={'auto'}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
 
-    var settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      autoplay: true,
-      initialSlide: 0,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            infinite: true,
-            dots: true
-          }
-        },
-        {
-          breakpoint: 950,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1,
-            initialSlide: 2
-          }
-        },
-        {
-          breakpoint: 640,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        }
-      ]
-    };
-    return (
-      <>
-        <div className="withButton">
-          <div className="TestimonialCarousel">
-              {/* <div className="first-errow grow">
-                <img className="slick-prev" src="./images/ArrowCircleDown.png" />
-              </div> */}
-            <Slider {...settings}>
-
-              <div className="TestimonialPage">
-                <div className="profilePages">
-                  <div>
-                    <div className="Profile">
-                      <img src="./images/men image.png" alt="profile" />
-                      <h1>Nick Weimann</h1>
-                      <p>District Directives Representative</p>
-                    </div>
-
-                    <div className="PointsofView">
-                      <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
-                    </div>
-
-                    <div className="star">
-                      <img src="./images/star 2.png" alt="stars" />
-                      <img src="./images/star 2.png" alt="stars" />
-                      <img src="./images/star 2.png" alt="stars" />
-                      <img src="./images/star 2.png" alt="stars" />
-                      <img src="./images/star 2.png" alt="stars" />
-                    </div>
-
-                    <div className="thumb">
-                      <img src="./images/thumb.png" alt="thumb" />
-                      <h1>Testimonial</h1>
-                    </div>
-                  </div>
+        loop={true}
+        pagination={false}
+        modules={[Autoplay,EffectCoverflow, Pagination]}
+        className="mySwiper"
+      >
+        
+          <SwiperSlide>
+            <div className="slidercontent">
+                <div><img src="./images/icons8-double-quotes-30.png" /></div>
+              <div className="view">
+                <div className="viewdescription">Starting Core created a fantastic landing page for our education brand that was tailored perfectly to our needs. The team was extremely professional, responsive, and efficient in their work. They took the time to understand our vision and goals and delivered a final product that exceeded our expectations. We highly recommend their services to anyone looking to improve their online presence.</div>
+              </div>
+              <div className="company">
+                <div>
+                  <h1>Anurag Mishra
+                  </h1>
+                  <h3>Founder / Cipher Schools</h3>
                 </div>
+                <img className="cipher" src="./images/client1.png" />
               </div>
 
-              <div className="TestimonialPage">
-                <div className="profilePages">
-                  <div>
-                    <div className="Profile">
-                      <img src="./images/men image.png" alt="profile" />
-                      <h1>Nick Weimann</h1>
-                      <p>District Directives Representative</p>
-                    </div>
-
-                    <div className="PointsofView">
-                      <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
-                    </div>
-
-                    <div className="star">
-                      <img src="./images/star 2.png" alt="stars" />
-                      <img src="./images/star 2.png" alt="stars" />
-                      <img src="./images/star 2.png" alt="stars" />
-                      <img src="./images/star 2.png" alt="stars" />
-                      <img src="./images/star 2.png" alt="stars" />
-                    </div>
-
-                    <div className="thumb">
-                      <img src="./images/thumb.png" alt="thumb" />
-                      <h1>Testimonial</h1>
-                    </div>
-                  </div>
+              {/* <hr /> */}
+            </div>
+          </SwiperSlide>
+       
+        <SwiperSlide>
+        <div className="slidercontent">
+                   <div><img src="./images/icons8-double-quotes-30.png" /></div>
+                 <div className="view">
+                   <div className="viewdescription">Thanks to Starting Core for creating our website in such a short time. We were impressed with the quality of the work and the responsiveness of the team. We appreciated the clear communication throughout the process and the attention to detail in the final product. We highly recommend Starting Core for a reliable  web design service.</div>
+                 </div>
+                 <div className="company">
+                   <div>
+                     <h1>Nagendra Singh Chauhan
+                     </h1>
+                     <h3>CEO / Vhrwf</h3>
+                   </div>
+                   <img className="ngo" src="./images/ngo.png" />
+                 </div>
+                 {/* <hr /> */}
+               </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className="slidercontent">
+                   <div><img src="./images/icons8-double-quotes-30.png" /></div>
+                 <div className="view">
+                   <div className="viewdescription">Got a website developed by the developers of Starting Core. Their service is really good and output submission was on time. Really satisfied with the web layout and efficiency. Their service is affordable and the developers have been cooperative throughout the project completion. Thankyou Starting Core, Mohit Pareek and Devraj Singh Rathore.</div>
+                 </div>
+                 <div className="company">
+                   <div>
+                     <h1>Parul and Aza
+                     </h1>
+                     <h3>Founder / Baba Azam</h3>
+                   </div>
+                   <img className="baba" src="./images/client3.png" />
+                 </div>
+               </div>      </SwiperSlide>      <SwiperSlide>        
+               <div className="slidercontent">
+                   <div><img src="./images/icons8-double-quotes-30.png" /></div>
+                 <div className="view">
+                   <div className="viewdescription">Had an amazing experience working with starting core. The best thing about them is that they work more as a team rather than an agency. Loved the UI/UX part of your work and also everything on the backend is top-notch. Starting Core is the go-to place for every founder who wants to build his/her web product.</div>
+                 </div>
+                 <div className="company">
+                   <div>
+                     <h1>Mukul</h1>
+                     <h3>Founder / kontent media</h3>
+                   </div>
+                   <img className="kontent" src="./images/Frame 1396.png" />
+                 </div>
+                 {/* <hr /> */}
+               </div>
+      </SwiperSlide>
+       
+          <SwiperSlide>
+            <div className="slidercontent">
+                <div><img src="./images/icons8-double-quotes-30.png" /></div>
+              <div className="view">
+                <div className="viewdescription">Starting Core created a fantastic landing page for our education brand that was tailored perfectly to our needs. The team was extremely professional, responsive, and efficient in their work. They took the time to understand our vision and goals and delivered a final product that exceeded our expectations. We highly recommend their services to anyone looking to improve their online presence.</div>
+              </div>
+              <div className="company">
+                <div>
+                  <h1>Anurag Mishra
+                  </h1>
+                  <h3>Founder / Cipher Schools</h3>
                 </div>
+                <img className="cipher" src="./images/client1.png" />
               </div>
 
-              <div className="TestimonialPage">
-                <div className="profilePages">
-                  <div>
-                    <div className="Profile">
-                      <img src="./images/men image.png" alt="profile" />
-                      <h1>Nick Weimann</h1>
-                      <p>District Directives Representative</p>
-                    </div>
-
-                    <div className="PointsofView">
-                      <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
-                    </div>
-
-                    <div className="star">
-                      <img src="./images/star 2.png" alt="stars" />
-                      <img src="./images/star 2.png" alt="stars" />
-                      <img src="./images/star 2.png" alt="stars" />
-                      <img src="./images/star 2.png" alt="stars" />
-                      <img src="./images/star 2.png" alt="stars" />
-                    </div>
-
-                    <div className="thumb">
-                      <img src="./images/thumb.png" alt="thumb" />
-                      <h1>Testimonial</h1>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="TestimonialPage">
-                <div className="profilePages">
-                  <div>
-                    <div className="Profile">
-                      <img src="./images/men image.png" alt="profile" />
-                      <h1>Nick Weimann</h1>
-                      <p>District Directives Representative</p>
-                    </div>
-
-                    <div className="PointsofView">
-                      <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
-                    </div>
-
-                    <div className="star">
-                      <img src="./images/star 2.png" alt="stars" />
-                      <img src="./images/star 2.png" alt="stars" />
-                      <img src="./images/star 2.png" alt="stars" />
-                      <img src="./images/star 2.png" alt="stars" />
-                      <img src="./images/star 2.png" alt="stars" />
-                    </div>
-
-                    <div className="thumb">
-                      <img src="./images/thumb.png" alt="thumb" />
-                      <h1>Testimonial</h1>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="TestimonialPage">
-                <div className="profilePages">
-                  <div>
-                    <div className="Profile">
-                      <img src="./images/men image.png" alt="profile" />
-                      <h1>Nick Weimann</h1>
-                      <p>District Directives Representative</p>
-                    </div>
-
-                    <div className="PointsofView">
-                      <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
-                    </div>
-
-                    <div className="star">
-                      <img src="./images/star 2.png" alt="stars" />
-                      <img src="./images/star 2.png" alt="stars" />
-                      <img src="./images/star 2.png" alt="stars" />
-                      <img src="./images/star 2.png" alt="stars" />
-                      <img src="./images/star 2.png" alt="stars" />
-                    </div>
-
-                    <div className="thumb">
-                      <img src="./images/thumb.png" alt="thumb" />
-                      <h1>Testimonial</h1>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="TestimonialPage">
-                <div className="profilePages">
-                  <div>
-                    <div className="Profile">
-                      <img src="./images/men image.png" alt="profile" />
-                      <h1>Nick Weimann</h1>
-                      <p>District Directives Representative</p>
-                    </div>
-
-                    <div className="PointsofView">
-                      <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
-                    </div>
-
-                    <div className="star">
-                      <img src="./images/star 2.png" alt="stars" />
-                      <img src="./images/star 2.png" alt="stars" />
-                      <img src="./images/star 2.png" alt="stars" />
-                      <img src="./images/star 2.png" alt="stars" />
-                      <img src="./images/star 2.png" alt="stars" />
-                    </div>
-
-                    <div className="thumb">
-                      <img src="./images/thumb.png" alt="thumb" />
-                      <h1>Testimonial</h1>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-            </Slider>
-              {/* <div className="second-errow grow">
-                <img className="slick-next" src="./images/ArrowCircleup(1).png" />
-              </div> */}
-          </div>
-          <div className="TestimonialButton">
-            <button className="TestimonialButton2" type="submit">&nbsp;&nbsp;Add Testimonial&nbsp;&nbsp;</button>
-          </div>
-        </div>
-
-      </>
-    );
-  }
+              {/* <hr /> */}
+            </div>
+          </SwiperSlide>
+       
+        <SwiperSlide>
+        <div className="slidercontent">
+                   <div><img src="./images/icons8-double-quotes-30.png" /></div>
+                 <div className="view">
+                   <div className="viewdescription">Thanks to Starting Core for creating our website in such a short time. We were impressed with the quality of the work and the responsiveness of the team. We appreciated the clear communication throughout the process and the attention to detail in the final product. We highly recommend Starting Core for a reliable  web design service.</div>
+                 </div>
+                 <div className="company">
+                   <div>
+                     <h1>Nagendra Singh Chauhan
+                     </h1>
+                     <h3>CEO / Vhrwf</h3>
+                   </div>
+                   <img className="ngo" src="./images/ngo.png" />
+                 </div>
+                 {/* <hr /> */}
+               </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div className="slidercontent">
+                   <div><img src="./images/icons8-double-quotes-30.png" /></div>
+                 <div className="view">
+                   <div className="viewdescription">Got a website developed by the developers of Starting Core. Their service is really good and output submission was on time. Really satisfied with the web layout and efficiency. Their service is affordable and the developers have been cooperative throughout the project completion. Thankyou Starting Core, Mohit Pareek and Devraj Singh Rathore.</div>
+                 </div>
+                 <div className="company">
+                   <div>
+                     <h1>Parul and Aza
+                     </h1>
+                     <h3>Founder / Baba Azam</h3>
+                   </div>
+                   <img className="baba" src="./images/client3.png" />
+                 </div>
+               </div>      </SwiperSlide>      <SwiperSlide>        
+               <div className="slidercontent">
+                   <div><img src="./images/icons8-double-quotes-30.png" /></div>
+                 <div className="view">
+                   <div className="viewdescription">Had an amazing experience working with starting core. The best thing about them is that they work more as a team rather than an agency. Loved the UI/UX part of your work and also everything on the backend is top-notch. Starting Core is the go-to place for every founder who wants to build his/her web product.</div>
+                 </div>
+                 <div className="company">
+                   <div>
+                     <h1>Mukul</h1>
+                     <h3>Founder / kontent media</h3>
+                   </div>
+                   <img className="kontent" src="./images/Frame 1396.png" />
+                 </div>
+                 {/* <hr /> */}
+               </div>
+      </SwiperSlide>
+       
+      </Swiper>
+      </div>
+    </>
+  );
 }
--- src/Landing.js
+++ src/Landing.js
@@ -243,68 +243,40 @@ function Landing() {
             src="./images/slide-4.jpg"
           />
         </div>
-
-        <h3 className="show1 animated">SCROLL DOWN</h3>
-        <img className="down1 animated" src="./images/down.png" />
       </div>
 
       {/* slider     ........................ */}
-      <section className="section-1">
-        <div className="text">
-          <div
-            className="side"
-            id="side1"
-            data-aos="fade-right"
-            data-aos-anchor-placement="top-center"
-          >
-            <h1>Our Team</h1>
+      <section className="founder">
+        <div>
+          <div className="founderheading">
+            <h1>Meet our Founder</h1>
           </div>
-          <br />
-          <p>
-            We love what we do and we do it with passion. We value the
-            experimentation of the message and smart incentives.
-          </p>
-        </div>
-        <div className="main-container">
-          <div className="box left">
-            <img src="./images/banner2.jpg" alt="" />
-            <div className="name">
-              <h2>Mohit Pareek</h2>
-              <span>Founder</span>
+          <div className="founderimage">
+            <div>
+              <img className="imagemohit" src="./images/founder.png" />
             </div>
-          </div>
-          <div className="box center">
-            <div className="container">
-              <Slider />
-            </div>
-            <div className="slide-controls">
-              <button id="prev-btn">
-                <div className="swiper-button-prev"></div>
-              </button>
-              <button id="next-btn">
-                <div className="swiper-button-next"></div>
-              </button>
-            </div>
-          </div>
+            <div>
+              <h1>Mohit Pareek</h1>
+              <h2>Founder</h2>
 
-          <div className="box right">
-            <img src="./images/banner2.jpg" alt="" />
-            <div className="name">
-              <h2>Team Starting Core</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              <div>
+                <a href="https://www.instagram.com/mohitpareek16/" title="Instagram"><img src="./images/InstagramLogo.png" alt="Instagram" /></a>
+                <a href="https://www.linkedin.com/in/mohit-pareek-b8a676204?originalSubdomain=in" title="Instagram"><img src="./images/LinkedinLogo.png" alt="Linkedin" /></a>
+              </div>
             </div>
           </div>
         </div>
-        <h3>Our team member is ready to help you!</h3>
       </section>
       {/* ........section............ */}
 
       <div id="foot">
         <div className="illust">
-          <img className="waves" src="./images/waves.png" alt="" />
-          <img className="i3" src="./images/-3.png" alt="" />
+         
           <img className="mans" src="./images/mans.png" alt="" />
-          <img className="i1" src="./images/-1.png" alt="" />
+         
         </div>
+        
         <footer>
           <div>
             <div className="col-1">
@@ -313,38 +285,42 @@ function Landing() {
                   <img src="./images/Img1.png" alt="" />
                   <h3 className="SC">Starting core</h3>
                 </div>
-                <p>
-                  Figma creates a new level of collaboration that never really
-                  existed before. The team is able to work together and ship
-                  products faster.”{" "}
-                </p>
+                <p>Figma creates a new level of collaboration that never really existed before. The team is able to work together and ship products faster.” </p>
               </div>
             </div>
 
             <div className="col-2">
-              <div className="col">
-                <h4>Services</h4>
-                <a href="#">Web Development</a>
-                <a href="#">App Development</a>
-                <a href="#">UI/UX Designer</a>
-                <a href="#">Graphic Designer</a>
-                <a href="#">SEO</a>
+              <div>
+                <div className="col">
+                  <h4>Services</h4>
+                  <a href="#">Web Development</a>
+                  <a href="#">App Development</a>
+                  <a href="#">UI/UX Designer</a>
+                  <a href="#">Graphic Designer</a>
+                  <a href="#">SEO</a>
+                </div>
+                <div className="col">
+                  <h4>Help</h4>
+                  {/* <!-- <a href="#">Help</a> --> */}
+                  <a href="#">Customer Support</a>
+                  <a href="#">Delivery Details</a>
+                  <a href="#">Terms & Conditions</a>
+                  <a href="/Policy">Privacy Policy</a>
+                </div>
+                <div className="col">
+                  <h4>Company</h4>
+                  <a href="#">Home</a>
+                  <a href="#">Why us?</a>
+                  <a href="#">Projects</a>
+                  <a href="#">Working Style</a>
+                  <a href="#">Contact Us</a>
+                </div>
               </div>
-              <div className="col">
-                <h4>Help</h4>
-                {/* <!-- <a href="#">Help</a> --> */}
-                <a href="#">Customer Support</a>
-                <a href="#">Delivery Details</a>
-                <a href="#">Terms & Conditions</a>
-                <a href="/Policy">Privacy Policy</a>
-              </div>
-              <div className="col">
-                <h4>Company</h4>
-                <a href="#">Home</a>
-                <a href="#">Why us?</a>
-                <a href="#">Projects</a>
-                <a href="#">Working Style</a>
-                <a href="#">Contact Us</a>
+              <div>
+                <div>
+                  <hr />
+                  <p>Copyright 2022 @startingcore All rights reserved.</p>
+                </div>
               </div>
             </div>
           </div>
